af4c511454bbd2e5d92ae18c9e9dcdac958aa506
Alexander Teshabaev <sasha.tesh@gmail.com>
2020-07-29 16:09:44 +0200

    removed API key example

--- examples/website-get-info.php
+++ examples/website-get-info.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use AnyComment\Api;
 use AnyComment\Config;
 
-$config = new Config('f4sBzGo_PX1fjuA75xaVwQh4FU4wPjWfLoR_hu7sq4KQkQiff4ZTBXKbqhbysxvX');
+$config = new Config('YOU_API_KEY');
 $api = new Api($config);
 
 var_dump($api->getWebsite()->getInfo());
